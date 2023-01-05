55a78afb0b6a286859037b006be0d67e0db5d454
toothemooon <35186738+toothemooon@users.noreply.github.com>
2023-01-05 20:33:19 +0800

    _

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,29 @@
 <script>
-	export let name;
+
+  // define your component tree before diving into
+  // App Header Footer Store {CreatePollForm, PollList -> PollDetails}
+  // UI component Button Card Tabs
+
+  import Header from './components/Header.svelte';
+  import Footer from './components/Footer.svelte';
+  import Tabs from './shared/Tabs.svelte';
+
+  // tabs
+  let items = ['Current Polls', 'Add New Poll'];
+  let activeItem = 'Current Polls'
 </script>
 
+<Header />
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Tabs />
+
+
 </main>
+<Footer />
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main{
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
---
+++ src/shared/Tabs.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let items;
+  export let activeItem;
+</script>
+
+<div class="tabs">
+  <!-- <ul>
+    {#each items as item}
+      <li>
+        <div>{item}</div>
+      </li>
+    {/each}
+  </ul> -->
+  <p>Check the component</p>
+</div>
+
+<style>
+
+</style>
